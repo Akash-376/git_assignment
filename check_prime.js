2e1ab9cda6284dafa547b77ff6dca56f78ca3820
Akash-376 <akchauhan376@gmail.com>
2022-09-20 11:35:21 +0530

    check prime code

---
+++ check_prime.js
@@ -0,0 +1,14 @@
+
+let N=13;
+let C = 0;
+
+for(let i=1;i<N;i++){
+    if(N%i==0){
+        count++;
+    }
+    if(count==2){
+        console.log("Prime");
+    }else{
+        console.log("Not";)
+    }
+}
